7b53e259ec756eb5a0ad4fefe997e134440444e0
durgacsm <vijayadurgakataru@gmail.com>
2026-09-07 13:58:29 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Hello {
+	public static void main(String[] args) {
+		int[] arr = {10, 20, 30, 40, 50};
+		int  value = arr[2];
+	    System.out.println("Value = " + value)	;
+		}
+	}
+
+
